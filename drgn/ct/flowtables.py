f18653fe035cda17107407e744324ce4833bc7d6
Chris Mi <cmi@nvidia.com>
2021-05-07 12:47:21 +0300

    update ct/flowtables.py

--- drgn/ct/flowtables.py
+++ drgn/ct/flowtables.py
@@ -21,12 +21,14 @@ for nf_ft in list_for_each_entry('struct nf_flowtable', flowtables.address_of_()
     cb_list = nf_ft.flow_block.cb_list
     for cb in list_for_each_entry('struct flow_block_cb', cb_list.address_of_(), 'list'):
 #         print(cb)
-        print("\tcb: %s" % address_to_name(hex(cb.cb)))
-        print("\tmlx5_ct_ft %lx" % cb.cb_priv)
         mlx5_ct_ft = Object(prog, 'struct mlx5_ct_ft *', address=cb.cb_priv.address_of_().value_())
-        print("\tmlx5_ct_ft.zone_restore_id: %d" % mlx5_ct_ft.zone_restore_id)
+        print("\t%s:" % mlx5_ct_ft.ct_priv.netdev.name.string_().decode(), end=' ')
+        print("\tcb: %s" % address_to_name(hex(cb.cb)), end='\t')
+        print("\tmlx5_ct_ft %lx" % cb.cb_priv, end='\t')
+#         print("\tmlx5_ct_ft.zone_restore_id: %d" % mlx5_ct_ft.zone_restore_id)
+        print('')
  
-    tuple_hash = nf_ft.rhashtable
-    for i, rhash in enumerate(hash(tuple_hash, 'struct flow_offload_tuple_rhash', 'node')):
-        print("flow_offload_tuple %lx" % rhash.tuple.address_of_())
-        print_tuple_rhash_tuple(rhash)
+#     tuple_hash = nf_ft.rhashtable
+#     for i, rhash in enumerate(hash(tuple_hash, 'struct flow_offload_tuple_rhash', 'node')):
+#         print("flow_offload_tuple %lx" % rhash.tuple.address_of_())
+#         print_tuple_rhash_tuple(rhash)
