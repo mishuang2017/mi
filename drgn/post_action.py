ad1e4bc0ff2f3227ef228d98572bc3216a9bb09d
Chris Mi <cmi@nvidia.com>
2021-06-22 11:11:55 +0300

    update

--- drgn/post_action.py
+++ drgn/post_action.py
@@ -13,4 +13,9 @@ from lib import *
 mlx5e_priv = get_mlx5e_priv(pf0_name)
 offloads = mlx5e_priv.mdev.priv.eswitch.offloads
 post_action = offloads.post_action
+print(post_action)
 flow_table("", post_action.ft)
+
+# for node in radix_tree_for_each(post_action.ids):
+#     print(node)
+#     mlx5_flow_attr = Object(prog, 'struct mlx5_flow_attr', address=node[1].value_())
