4ecd6c6875edd3b7dcd0f5837ae62be703bce6df
Chris Mi <cmi@nvidia.com>
2021-08-06 09:59:14 +0800

    update

--- drgn/post_action.py
+++ drgn/post_action.py
@@ -10,9 +10,8 @@ import os
 sys.path.append(".")
 from lib import *
 
-mlx5e_priv = get_mlx5e_priv(pf0_name)
-offloads = mlx5e_priv.mdev.priv.eswitch.offloads
-post_action = offloads.post_action
+mlx5e_rep_priv = get_mlx5e_rep_priv()
+post_action = mlx5e_rep_priv.uplink_priv.post_action
 # print(post_action)
 flow_table("", post_action.ft)
 
