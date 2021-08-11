aa2a03ab9e0039c4c8f6729846f59b4796fb45bd
Chris Mi <cmi@nvidia.com>
2021-08-11 04:51:04 +0300

    update

--- mod/hello/hello.c
+++ mod/hello/hello.c
@@ -1,22 +1,18 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/rcu_node_tree.h>
+#include <net/ip6_fib.h>
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 static int hello_init(void)
 {
-	atomic_t a;
-	printk(KERN_ALERT "hello: %lx\n", sizeof(a));
+	struct fib6_entry_notifier_info info;
+	struct fib6_info rt;
 
-/* 	printk(KERN_ALERT "hello: NUM_RCU_NODES, %d\n", NUM_RCU_NODES); */
-/* 	printk(KERN_ALERT "hello: NUM_RCU_LVL_0, %d\n", NUM_RCU_LVL_0); */
-/* 	printk(KERN_ALERT "hello: NUM_RCU_LVL_2, %d\n", NUM_RCU_LVL_1); */
-/* 	printk(KERN_ALERT "hello: NR_CPUS, %d\n", NR_CPUS); */
-/* 	printk(KERN_ALERT "hello: RCU_FANOUT_1, %d\n", RCU_FANOUT_1); */
-/* 	printk(KERN_ALERT "hello: RCU_FANOUT_2, %d\n", RCU_FANOUT_2); */
-/* 	printk(KERN_ALERT "hello: RCU_FANOUT_3, %d\n", RCU_FANOUT_3); */
-/* 	printk(KERN_ALERT "hello: RCU_FANOUT_4, %d\n", RCU_FANOUT_4); */
+	info.rt = &rt;
+	info.rt->fib6_dst.plen = 0;
+
 	return 0;
 }
 
