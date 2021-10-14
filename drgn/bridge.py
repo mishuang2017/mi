b7e4de99677cd2cbe7d8ffc1fbb2490c861297e9
Chris Mi <cmi@nvidia.com>
2021-10-14 05:42:02 +0300

    update

--- drgn/bridge.py
+++ drgn/bridge.py
@@ -15,8 +15,12 @@ mlx5_eswitch = mlx5e_priv.mdev.priv.eswitch
 mlx5_esw_bridge_offloads = mlx5_eswitch.br_offloads
 # print(mlx5_esw_bridge_offloads)
 
+# ingress_ft is global
+# egress_ft is per bridge
+
 print("=== mlx5_esw_bridge_offloads ingress ===")
 ingress_ft = mlx5_esw_bridge_offloads.ingress_ft
+print("ingress_ft %x" % ingress_ft.value_())
 flow_table("ingress_ft", ingress_ft)
 
 # skip_ft = mlx5_esw_bridge_offloads.skip_ft
@@ -25,7 +29,7 @@ flow_table("ingress_ft", ingress_ft)
 print("\n=== mlx5_esw_bridge egress ===")
 
 def print_mlx5_esw_bridge(bridge):
-#     print(bridge)
+    print("bridge.ifindex: %d" % bridge.ifindex)
     egress_ft = bridge.egress_ft
     flow_table("egress_ft", egress_ft)
 
@@ -41,12 +45,21 @@ bridges = mlx5_esw_bridge_offloads.bridges
 for mlx5_esw_bridge in list_for_each_entry('struct mlx5_esw_bridge', bridges.address_of_(), 'list'):
     print_mlx5_esw_bridge(mlx5_esw_bridge)
 
-sys.exit(0)
+# ports = mlx5_esw_bridge_offloads.ports
+# for node in radix_tree_for_each(ports):
+#     port = Object(prog, 'struct mlx5_esw_bridge_port', address=node[1].value_())
+#     mlx5_esw_bridge = port.bridge
+#     print_mlx5_esw_bridge(mlx5_esw_bridge)
 
-ports = mlx5_esw_bridge_offloads.ports
-for node in radix_tree_for_each(ports):
-    port = Object(prog, 'struct mlx5_esw_bridge_port', address=node[1].value_())
-#     print(port)
-    mlx5_esw_bridge = port.bridge
-#     print(mlx5_esw_bridge)
-    print_mlx5_esw_bridge(mlx5_esw_bridge)
+print("\n=== switchdev_notif_chain ==\n")
+switchdev_notif_chain = prog['switchdev_notif_chain']
+notifier_block = switchdev_notif_chain.head
+
+while True:
+    if notifier_block.value_() == 0:
+        break
+    print(notifier_block.notifier_call)
+    if notifier_block.notifier_call.value_() == prog['mlx5_esw_bridge_switchdev_event'].address_of_().value_():
+        mlx5_esw_bridge_offloads = container_of(notifier_block, "struct mlx5_esw_bridge_offloads", "nb")
+#         print(mlx5_esw_bridge_offloads)
+    notifier_block = notifier_block.next
