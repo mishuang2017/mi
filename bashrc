4e7de8103c92d0951cd05c1596dd4a5de59d4080
Chris Mi <cmi@nvidia.com>
2022-03-24 19:36:25 +0800

    update

--- bashrc
+++ bashrc
@@ -10583,9 +10583,8 @@ else
 	export CONFIG=/workspace/dev_reg_conf.sh
 fi
 
-test1=test-bridge-egress-miss-restore.sh
-test1=test-vf-vf-ping-legacy-vport-match.sh
-test1=test-vf-lag-nic-tc-rule.sh
+test1=test-ecmp-route-events.sh
+test1=test-ecmp-add-multipath-rule.sh
 alias test1="export CONFIG=config_chrism_cx5.sh; ./$test1"
 alias test2="export CONFIG=/workspace/dev_reg_conf.sh; cd /workspace/asap_dev_test; RELOAD_DRIVER_PER_TEST=1; ./$test1"
 alias test2="export CONFIG=/workspace/dev_reg_conf.sh; cd /workspace/asap_dev_test; ./$test1"
--- drgn/mlx5_lag.py
+++ drgn/mlx5_lag.py
@@ -12,8 +12,18 @@ from lib import *
 
 mlx5e_priv = get_mlx5e_priv(pf0_name)
 mlx5_lag = mlx5e_priv.mdev.priv.lag
-print(mlx5_lag)
+fib_nh = mlx5_lag.lag_mp.mfi.fib_nh[0]
+print(fib_nh)
+print("fib_nh.nh_common.nhc_dev.name: %s" % fib_nh.nh_common.nhc_dev.name.string_().decode())
+# print("fib_nh.nh_common.nhc_gw: %s" % ipv4(ntohl(fib_nh.nh_common.nhc_gw.ipv4)))
+print("gw: %s" % ipv4(socket.ntohl(fib_nh.nh_common.nhc_gw.ipv4.value_())))
 print("flags: %x" % mlx5_lag.flags)
+
+# mlx5e_priv = get_mlx5e_priv(pf1_name)
+# mlx5_lag = mlx5e_priv.mdev.priv.lag
+# print(mlx5_lag.lag_mp)
+# print("flags: %x" % mlx5_lag.flags)
+
 
 MLX5_LAG_FLAG_ROCE = prog['MLX5_LAG_FLAG_ROCE']
 MLX5_LAG_FLAG_SRIOV = prog['MLX5_LAG_FLAG_SRIOV']
