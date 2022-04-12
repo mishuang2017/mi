97c46ed619e2443287dd352f1c44d74ffff9331a
Chris Mi <cmi@nvidia.com>
2022-04-12 04:13:31 +0300

    update

--- drgn/test1.py
+++ drgn/test1.py
@@ -12,5 +12,14 @@ from lib import *
 
 mlx5e_priv = get_mlx5e_priv(pf0_name)
 esw = mlx5e_priv.mdev.priv.eswitch
+# print(mlx5e_priv.mdev.priv.eswitch)
 print("mode: %d" % esw.mode)
 print(esw.user_count)
+print(mlx5e_priv.ppriv)
+
+
+mlx5e_priv = get_mlx5e_priv(pf1_name)
+esw = mlx5e_priv.mdev.priv.eswitch
+print("mode: %d" % esw.mode)
+print(esw.user_count)
+print(mlx5e_priv.ppriv)
