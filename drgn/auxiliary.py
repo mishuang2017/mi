f9fc5c53743ab00d1261bd0f81447b5c37af81a3
Chris Mi <cmi@nvidia.com>
2022-04-15 09:55:12 +0300

    auxiliary.py

--- drgn/auxiliary.py
+++ drgn/auxiliary.py
@@ -12,47 +12,31 @@ libpath = os.path.dirname(os.path.realpath("__file__"))
 sys.path.append(libpath)
 import lib
 
-devs = {}
+def print_bus(bus):
+    print("===================== %s ==========================" % bus)
+    bus_type = prog[bus]
+    subsys_private = bus_type.p
+    k_list = subsys_private.drivers_kset
 
+    print("=== driver ===")
+    klist_drivers = subsys_private.klist_drivers
+    for driver_private in list_for_each_entry('struct driver_private', klist_drivers.k_list.address_of_(), 'knode_bus.n_node'):
+        print('----------------------------------')
+        name = driver_private.kobj.name.string_().decode()
+        print(name)
+        if bus == "auxiliary_bus_type" and name.find("mlx5_core") == 0:
+            auxiliary_driver = container_of(driver_private.driver, "struct auxiliary_driver", "driver")
+            print(auxiliary_driver.name.string_().decode())
+    print('')
 
-# bus_type = prog["pci_bus_type"]
-# subsys_private = bus_type.p
-# k_list = subsys_private.drivers_kset
+    print("=== device ===")
+    klist_devices = subsys_private.klist_devices
+    for device_private in list_for_each_entry('struct device_private', klist_devices.k_list.address_of_(), 'knode_bus.n_node'):
+        print(device_private.device.kobj.name.string_().decode())
+    print('')
 
-# for kobject in list_for_each_entry('struct kobject', k_list.list.address_of_(), 'entry'):
-#     print(kobject)
-
-# klist_drivers = subsys_private.klist_drivers
-# for driver_private in list_for_each_entry('struct driver_private', klist_drivers.k_list.address_of_(), 'knode_bus.n_node'):
-#     name = driver_private.kobj.name.string_().decode()
-#     if name.find("mlx5_core") == 0:
-#         print(driver_private)
-#         auxiliary_driver = container_of(driver_private.driver, "struct auxiliary_driver", "driver")
-#         print(auxiliary_driver)
-
-bus_type = prog["auxiliary_bus_type"]
-subsys_private = bus_type.p
-k_list = subsys_private.drivers_kset
-
-# for kobject in list_for_each_entry('struct kobject', k_list.list.address_of_(), 'entry'):
-#     print(kobject)
-
-print("=== driver ===")
-klist_drivers = subsys_private.klist_drivers
-for driver_private in list_for_each_entry('struct driver_private', klist_drivers.k_list.address_of_(), 'knode_bus.n_node'):
-    name = driver_private.kobj.name.string_().decode()
-    if name.find("mlx5_core") == 0:
-        auxiliary_driver = container_of(driver_private.driver, "struct auxiliary_driver", "driver")
-        print(auxiliary_driver.name.string_().decode())
-
-print("=== device ===")
-klist_devices = subsys_private.klist_devices
-for device_private in list_for_each_entry('struct device_private', klist_devices.k_list.address_of_(), 'knode_bus.n_node'):
-#     name = device_private.kobj.name.string_().decode()
-#     if name.find("mlx5_core") == 0:
-    print(device_private.device.kobj.name.string_().decode())
-#     auxiliary_driver = container_of(driver_private.driver, "struct auxiliary_driver", "driver")
-#     print(auxiliary_driver)
+# print_bus("pci_bus_type")
+print_bus("auxiliary_bus_type")
 
 # mlx5e_rep_driver = prog['mlx5e_rep_driver']
 # print(mlx5e_rep_driver)
