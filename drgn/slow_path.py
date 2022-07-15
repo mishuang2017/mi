310ac2519c6e5665761d43d3675acf37d8d45992
Chris Mi <cmi@nvidia.com>
2022-07-15 07:43:23 +0300

    update

--- drgn/slow_path.py
+++ drgn/slow_path.py
@@ -19,7 +19,13 @@ mlx5_eswitch_fdb = mlx5e_priv.mdev.priv.eswitch.fdb_table
 slow_fdb = mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.slow_fdb
 flow_table("mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.slow_fdb", slow_fdb)
 
-# sys.exit(0)
+miss_meter_fdb = mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.miss_meter_fdb
+flow_table("mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.miss_meter_fdb", miss_meter_fdb)
+
+post_miss_meter_fdb = mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.post_miss_meter_fdb
+flow_table("mlx5e_priv.mdev.priv.eswitch.fdb_table.offloads.post_miss_meter_fdb", post_miss_meter_fdb)
+
+sys.exit(0)
 vport_to_tir = mlx5e_priv.mdev.priv.eswitch.offloads.ft_offloads
 flow_table("mlx5e_priv.mdev.priv.eswitch.offloads.ft_offloads", vport_to_tir)
 
