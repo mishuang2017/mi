10c287719ccbb991f9bf54f6fe41f8e6cea61045
Chris Mi <cmi@nvidia.com>
2022-07-22 04:20:51 +0300

    devcom.py mlx5_vport_egress.py

---
+++ drgn/devcom.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/drgn -k
+
+from drgn.helpers.linux import *
+from drgn import Object
+import time
+import socket
+import sys
+import os
+
+sys.path.append(".")
+from lib import *
+
+mlx5e_priv = get_mlx5e_priv(pf0_name)
+devcom = mlx5e_priv.mdev.priv.devcom
+print(devcom.priv)
+
+ mlx5e_priv = get_mlx5e_priv(pf1_name)
+# devcom = mlx5e_priv.mdev.priv.devcom
+# print(devcom.priv)
