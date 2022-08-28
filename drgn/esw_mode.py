321019b16a5177fb3898b8628e6f0e80f7b93263
Chris Mi <cmi@nvidia.com>
2022-08-28 06:10:32 +0300

    update

--- drgn/esw_mode.py
+++ drgn/esw_mode.py
@@ -16,15 +16,15 @@ def print_esw(priv):
     print("mlx5_core_dev %#x" % priv.mdev)
     print("esw->flags: %#x" % esw.flags)
     print("mode: %d" % esw.mode)
-    print("fdb_table.flags: %x" % esw.fdb_table.flags);
-    print("total_vports: %d" % esw.total_vports)
-    print("enabled_vports: %d" % esw.enabled_vports)
+    print("esw->fdb_table->flags: %x" % esw.fdb_table.flags);
+    print("esw->total_vports: %d" % esw.total_vports)
+    print("esw->enabled_vports: %d" % esw.enabled_vports)
     print("esw->manager_vport: %d" % esw.manager_vport)
-    print("esw->esw_funcs.num_vfs: %d" % esw.esw_funcs.num_vfs)
-    print("esw->dev->priv.sriov.num_vfs: %d" % esw.dev.priv.sriov.num_vfs)
-    print("esw.fdb_table.offloads.send_to_vport_meta_grp: %x" % esw.fdb_table.offloads.send_to_vport_meta_grp)
-    print("esw->fdb_table.offloads.send_to_vport_meta_rules: %d" % esw.fdb_table.offloads.send_to_vport_meta_rules)
-    print("esw->offloads.inline_mode: %d" % esw.offloads.inline_mode)
+    print("esw->esw_funcs->num_vfs: %d" % esw.esw_funcs.num_vfs)
+    print("esw->dev->priv->sriov.num_vfs: %d" % esw.dev.priv.sriov.num_vfs)
+    print("esw->fdb_table->offloads->send_to_vport_meta_grp: %x" % esw.fdb_table.offloads.send_to_vport_meta_grp)
+    print("esw->fdb_table->offloads->send_to_vport_meta_rules: %d" % esw.fdb_table.offloads.send_to_vport_meta_rules)
+    print("esw->offloads->inline_mode: %d" % esw.offloads.inline_mode)
     print(esw.user_count)
     print(esw.offloads.num_flows)
 
