8aaf291deb341a314cf2dba85c0c192bb3a942ca
Chris Mi <cmi@nvidia.com>
2024-08-28 09:55:10 +0300

    devcom.py

--- drgn/devcom.py
+++ drgn/devcom.py
@@ -10,29 +10,25 @@ import os
 sys.path.append(".")
 from lib import *
 
-print(" ==== devcom_list ==== ")
-devcom_list = prog['devcom_list']
-# for devcom in list_for_each_entry('struct mlx5_devcom_list', devcom_list.address_of_(), 'list'):
-#     print(devcom)
+MLX5_DEVCOM_ESW_OFFLOADS = prog['MLX5_DEVCOM_ESW_OFFLOADS']
 
-# exit(0)
+print(" ==== devcom_dev_list ==== ")
+devcom_dev_list = prog['devcom_dev_list']
+for devcom in list_for_each_entry('struct mlx5_devcom_dev', devcom_dev_list.address_of_(), 'list'):
+    pci_name = devcom.dev.device.kobj.name.string_().decode()
+    print('--------------------------------------')
+    print(pci_name)
+    print(devcom)
 
-print(" ===== port 1 =====")
-
-mlx5e_priv = get_mlx5e_priv(pf0_name)
-devcom = mlx5e_priv.mdev.priv.devcom
-print("devom.idx: %d" % devcom.idx)
-# print(devcom.priv)
-# print(devcom.priv.components[0].device)
-esw = Object(prog, 'struct mlx5_eswitch *', address=devcom.priv.components[0].device[0].data)
-print("%x" % esw.dev.address_of_())
-esw = Object(prog, 'struct mlx5_eswitch *', address=devcom.priv.components[0].device[1].data)
-print("%x" % esw.dev.address_of_())
-print(devcom.priv.devs)
-
-print(" ===== port 2 =====")
-
-mlx5e_priv = get_mlx5e_priv(pf1_name)
-devcom = mlx5e_priv.mdev.priv.devcom
-print("devom.idx: %d" % devcom.idx)
-# print(devcom.priv)
+print(" ==== devcom_comp_list ==== ")
+devcom_comp_list = prog['devcom_comp_list']
+for devcom in list_for_each_entry('struct mlx5_devcom_comp', devcom_comp_list.address_of_(), 'comp_list'):
+    print(devcom)
+    if devcom.id != MLX5_DEVCOM_ESW_OFFLOADS:
+        continue
+    for dev in list_for_each_entry('struct mlx5_devcom_comp_dev', devcom.comp_dev_list_head.address_of_(), 'list'):
+        print(dev)
+        esw = Object(prog, 'struct mlx5_eswitch', address=dev.data)
+#         print(esw)
+        pci_name = esw.dev.device.kobj.name.string_().decode()
+        print(pci_name)
